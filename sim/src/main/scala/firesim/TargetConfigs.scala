023a2c112295a84fbcb76aea80589b5b407ab76e
Abraham Gonzalez <abe.j.gonza@gmail.com>
2018-11-16 06:17:29 +0000

    Update Spec17 makefile and Speckle | Update target for securechip

--- sim/src/main/scala/firesim/TargetConfigs.scala
+++ sim/src/main/scala/firesim/TargetConfigs.scala
@@ -145,8 +145,19 @@ class FireSimBoomConfig extends Config(
   new WithBlockDevice ++
   new WithBoomL2TLBs(1024) ++
   new WithBoomSynthAssertExcludes ++ // Will do nothing unless assertion synth is enabled
-  // Using a small config because it has 64-bit system bus, and compiles quickly
   new boom.system.BoomConfig)
+
+class FireSimSecureBoomConfig extends Config(
+  new WithBootROM ++
+  new WithPeripheryBusFrequency(BigInt(3200000000L)) ++
+  new WithExtMemSize(0x400000000L) ++ // 16GB
+  new WithoutTLMonitors ++
+  new WithUARTKey ++
+  new WithNICKey ++
+  new WithBlockDevice ++
+  new WithBoomL2TLBs(1024) ++
+  new WithBoomSynthAssertExcludes ++ // Will do nothing unless assertion synth is enabled
+  new boom.system.SecureBoomConfig)
 
 // A safer implementation than the one in BOOM in that it
 // duplicates whatever BOOMTileKey.head is present N times. This prevents
